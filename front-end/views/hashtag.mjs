59760e8169f09727b8aa49a264051f6d7746b7b8
sheetalkharab <sheetalrana89@gmail.com>
2025-09-29 15:04:45 +0100

    update in code for hashtag slow down

--- front-end/views/hashtag.mjs
+++ front-end/views/hashtag.mjs
@@ -1,4 +1,4 @@
-import {renderOne, renderEach, destroy} from "../lib/render.mjs";
+import { renderOne, renderEach, destroy } from "../lib/render.mjs";
 import {
   state,
   apiService,
@@ -7,17 +7,19 @@ import {
   getTimelineContainer,
   getHeadingContainer,
 } from "../index.mjs";
-import {createLogin, handleLogin} from "../components/login.mjs";
-import {createLogout, handleLogout} from "../components/logout.mjs";
-import {createBloom} from "../components/bloom.mjs";
-import {createHeading} from "../components/heading.mjs";
+import { createLogin, handleLogin } from "../components/login.mjs";
+import { createLogout, handleLogout } from "../components/logout.mjs";
+import { createBloom } from "../components/bloom.mjs";
+import { createHeading } from "../components/heading.mjs";
 
 // Hashtag view: show all tweets containing this tag
 
 function hashtagView(hashtag) {
+  const formattedHashtag = hashtag.startsWith("#") ? hashtag : `#${hashtag}`;
   destroy();
-
-  apiService.getBloomsByHashtag(hashtag);
+  if (formattedHashtag !== state.currentHashtag) {
+    apiService.getBloomsByHashtag(hashtag);
+  }
 
   renderOne(
     state.isLoggedIn,
@@ -52,4 +54,4 @@ function hashtagView(hashtag) {
   );
 }
 
-export {hashtagView};
+export { hashtagView };
