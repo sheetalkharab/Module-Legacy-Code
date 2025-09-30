f359aaf56d1d6d5e17150436d8b99125fff6caf8
sheetalkharab <sheetalrana89@gmail.com>
2025-09-30 21:26:17 +0100

    add unfollow function

--- backend/data/follows.py
+++ backend/data/follows.py
@@ -41,3 +41,11 @@ def get_inverse_followed_usernames(followee: User) -> List[str]:
         )
         rows = cur.fetchall()
         return [row[0] for row in rows]
+    
+def unfollow(follower: User, followee: User):
+    with db_cursor() as cur:
+        cur.execute(
+            "DELETE FROM follows WHERE follower = %s AND followee = %s",
+            (follower.id, followee.id),
+        )
+
