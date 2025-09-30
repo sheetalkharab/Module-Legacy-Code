70ea242f2a9036b97afa3a24f23b392b86311ad5
sheetalkharab <sheetalrana89@gmail.com>
2025-09-30 21:25:49 +0100

    add api url for unfollow

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,7 @@ from endpoints import (
     send_bloom,
     suggested_follows,
     user_blooms,
+    do_unfollow,
 )
 
 from dotenv import load_dotenv
@@ -54,6 +55,8 @@ def main():
     app.add_url_rule("/profile", view_func=self_profile)
     app.add_url_rule("/profile/<profile_username>", view_func=other_profile)
     app.add_url_rule("/follow", methods=["POST"], view_func=do_follow)
+    app.add_url_rule("/unfollow/<username>", view_func=do_unfollow, methods=["POST"])
+
     app.add_url_rule("/suggested-follows/<limit_str>", view_func=suggested_follows)
 
     app.add_url_rule("/bloom", methods=["POST"], view_func=send_bloom)
